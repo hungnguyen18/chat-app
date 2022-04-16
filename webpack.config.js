0e988273e1654cce02e3273d5272deabc6e2e609
hung <hungtrewq3214@gmail.com>
2022-04-16 17:08:38 +0700

    test deloy

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // Dẫn tới file index.js ta đã tạo
+    // The entry point file described above
+    entry: './src/index.js',
+    // The location of the build folder described above
     output: {
-        path: path.join(__dirname, '/build'), // Thư mục chứa file được build ra
-        filename: 'bundle.js', // Tên file được build ra
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        exclude: /node_modules\/@firebase\/auth/, //to just exclude firebase auth from source-map
     },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/, // Sẽ sử dụng babel-loader cho những file .js
-                loader: 'source-map-loader',
-                exclude: /node_modules/, // Loại trừ thư mục node_modules
-                exclude: /node_modules\/@firebase\/auth/, //to just exclude firebase auth from source-map
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    // Chứa các plugins sẽ cài đặt trong tương lai
-    plugins: [],
+    // Optional and for development only. This provides the ability to
+    // map the built code back to the original source format when debugging.
+    devtool: 'eval-source-map',
 };
